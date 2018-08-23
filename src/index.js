67fc38a0e075730234e50359404989404510a164
ptavarez16 <ptavarez16@yahoo.com>
2018-08-23 11:11:10 -0400

    Add YouTube Search and search bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBvAM36HwBlBj1wif-qFFfSKwzeWoW7KrQ';
+
 // Create a mew component that produces HTML
 const App = () => {
-  return <div>What's up my G?!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it in the DOM
