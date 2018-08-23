898fbd5a14c5167b1e68397f22a38f11738e5497
ptavarez16 <ptavarez16@yahoo.com>
2018-08-23 14:27:24 -0400

    Add video list and video list items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyBvAM36HwBlBj1wif-qFFfSKwzeWoW7KrQ';
 
-// Create a mew component that produces HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      
+      this.state = { videos: [] };
+      
+      YTSearch({key: API_KEY, term: 'YuGiOh'}, (videos) => {
+        this.setState({ videos });
+      });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
