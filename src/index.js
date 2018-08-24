0b515e15adde3c5160df9abbc80b59afd1a3569b
ptavarez16 <ptavarez16@yahoo.com>
2018-08-24 17:45:17 -0400

    Pass empty string into this.videoSearch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,7 +18,7 @@ class App extends Component {
         selectedVideos: null
        };
        
-      this.videoSearch('Anime');
+      this.videoSearch('');
   }
 
   videoSearch(term) {
@@ -30,9 +31,10 @@ class App extends Component {
   }
   
   render() {
+    const videoSearch = _.debounce(term => this.videoSearch(term), 500);
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideos}/>
         <VideoList
           onVideoSelect={selectedVideos => this.setState({selectedVideos})}
